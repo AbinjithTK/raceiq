f6f7e3db9786577ac535fa3ad5ca24f67beabd2d
abinjith <abinjith87@gmail.com>
2025-11-27 12:38:22 +0530

    Update frontend to use Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://raceiq-backend-1091035104912.us-central1.run.app'
+const API_BASE_URL = 'https://raceiq-backend.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
